18ce3e74be7383bc6cf0ba57a8663ec8b61bb480
Racing Icemen <rkkommu@ucdavis.edu>
2020-10-20 07:08:51 -0700

    Completed chapter 2

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,8 +1,16 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, coordinate = %Coordinate{}) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(guesses = %Guesses{}, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
+  end
 end
