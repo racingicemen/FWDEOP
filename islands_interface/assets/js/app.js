eb67cb57264439e359d80587fbc592d978fe64ef
Racing Icemen <rkkommu@ucdavis.edu>
2020-12-14 07:06:54 -0800

    Code upto adding a second player from the browser

--- islands_interface/assets/js/app.js
+++ islands_interface/assets/js/app.js
@@ -13,3 +13,53 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+var phoenix = require("phoenix");
+var socket = new phoenix.Socket("/socket", {});
+window.kconnect = () => socket.connect();
+window.kprint = (str) => console.log(str);
+window.new_channel = (subtopic, screen_name) => {
+    return socket.channel("game:" + subtopic, {screen_name: screen_name});
+}
+window.join = (channel) => {
+    channel.join()
+        .receive("ok", response => {
+            console.log("Joined successfully", response)
+        })
+        .receive("error", response => {
+            console.log("Unable to join", response)
+        })
+}
+window.leave = (channel) => {
+    channel.leave()
+        .receive("ok", response => {
+            console.log("Left successfully", response)
+        })
+        .receive("error", response => {
+            console.log("Unable to leave", response)
+        })
+}
+window.say_hello = (channel, greeting) => {
+    channel.push("hello", {"message": greeting})
+        .receive("ok", response => {
+            console.log("Hello", response.message)
+        })
+        .receive("error", response => {
+            console.log("Unable to say hello to the channel.", response.message)
+        })
+}
+window.new_game = (channel) => {
+    channel.push("new_game")
+        .receive("ok", response => {
+            console.log("New Game!", response)
+        })
+        .receive("error", response => {
+            console.log("Unable to start a new game", response)
+        })
+}
+window.add_player = (channel, player) => {
+    channel.push("add_player", player)
+        .receive("error", response => {
+            console.log("Unable to add player: " + player, response)
+        })
+}
